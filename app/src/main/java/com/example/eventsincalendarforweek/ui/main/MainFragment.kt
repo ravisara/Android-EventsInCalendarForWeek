bf3445da96af6ecc807cfb5cf359de52f9cef07d
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-05-03 10:39:23 +0100

    Gets the fragment to show some dummy data in the recyclerview using the newly created adapter for the recyclerview.

--- app/src/main/java/com/example/eventsincalendarforweek/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/eventsincalendarforweek/ui/main/MainFragment.kt
@@ -8,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eventsincalendarforweek.R
 import com.example.eventsincalendarforweek.repository.RetrofitInstance
 import java.io.IOException
@@ -22,6 +24,7 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var theRecyclerView : RecyclerView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -42,10 +45,29 @@ class MainFragment : Fragment() {
             Log.i(TAG, response.toString())
         }
 
+        setupRecyclerView()
+
     }
 
-    private fun setupRecyclerView() {
+    /**
+     * Called immediately after [.onCreateView]
+     * has returned, but before any saved state has been restored in to the view.
+     * This gives subclasses a chance to initialize themselves once
+     * they know their view hierarchy has been completely created.  The fragment's
+     * view hierarchy is not however attached to its parent at this point.
+     * @param view The View returned by [.onCreateView].
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        theRecyclerView = view.findViewById(R.id.eventsInWeekRecyclerView)
+    }
 
+    private fun setupRecyclerView() = theRecyclerView.apply {
+        val dummyDataSet = listOf<String>("event 1", "event 2", "event 3")
+        adapter = CustomAdapterForRecyclerView(dummyDataSet)
+        layoutManager = LinearLayoutManager(requireContext())
     }
 
 
