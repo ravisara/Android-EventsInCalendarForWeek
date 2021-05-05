6d67cb75ca780dca9385c84c33ad4874ce42ea1b
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-05-05 08:00:11 +0100

    Adds a method to the BuzzHireAPI interface to obtain events between two date time instants passed.

--- app/src/main/java/com/example/eventsincalendarforweek/repository/BuzzHireAPI.kt
+++ app/src/main/java/com/example/eventsincalendarforweek/repository/BuzzHireAPI.kt
@@ -2,10 +2,15 @@ package com.example.eventsincalendarforweek.repository
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BuzzHireAPI {
 
     @GET(value = "/calendar/?format=json")
-    suspend fun getAllCalendarEvents(): Response<CalendarEvents> // coroutines are used
+    suspend fun getAllCalendarEvents(): Response<CalendarEvents> // suspend as coroutines are used
+
+    @GET(value = "/calendar/?format=json")
+    suspend fun getCalendarEventsInSpecifiedInterval(@Query("since") startDateTime: String, @Query("before") endDateTime: String): Response<CalendarEvents>
 
 }
