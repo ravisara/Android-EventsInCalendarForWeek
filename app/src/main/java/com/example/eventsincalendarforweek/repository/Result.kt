7160cf2e7f513b618fd248a6f0cdf47dcc2ae681
Ravisara S. Kariyawasam <ravisara@gmail.com>
2021-05-05 08:05:30 +0100

    Changes the class that holds calendar event data received from the API query so that the same class can be used for storing formatted data. Changes from immutable properties to mutable ones.
    Also adds a new property that is later used for sorting the results by date and time.

--- app/src/main/java/com/example/eventsincalendarforweek/repository/Result.kt
+++ app/src/main/java/com/example/eventsincalendarforweek/repository/Result.kt
@@ -1,9 +1,11 @@
 package com.example.eventsincalendarforweek.repository
 
+// All these variables could have been val s but had to make them var as start and startDateTimeWithOnlyDigitsForSorting fields are replaced later on with a different format of the respective values.
 data class Result(
-        val id: Int,
-        val start: String,
-        val end: String,
-        val label: String,
-        val category: String
+        var id: Int,
+        var start: String,
+        var startDateTimeWithOnlyDigitsForSorting: String?, // this is not coming from the API. this is used later on for sorting in ascending order of start date time. Not ideal, but quite handy.
+        var end: String,
+        var label: String,
+        var category: String
 )
